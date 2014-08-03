14e14313bf37d595b99ab59bc28df1b7a4077c2f
Insidexa <nostresss77@gmail.com>
2014-08-03 11:52:50 +0300

    Update createData.java

--- createData.java
+++ createData.java
@@ -1,22 +1,77 @@
 import java.io.File;
+import java.io.FileReader;
 import java.io.FilenameFilter;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
 
 public class CreateData {
 
     private   String downloads        = "";
-    protected String[] files;
-    protected String path;
-    private   String[] archives       = new String[] {".gzip", ".zip", ".rar", ".tar.gz", ".7z", ".tgz", ".tar.xz"};
-    private   String[] music          = new String[] {".mp3", ".waw"};
-    private   String[] android        = new String[] {".apk"};
-    private   String[] web            = new String[] {".php", ".js", ".html", ".phtml", ".htm", ".json"};
-    private   String[] images         = new String[] {".ico", ".jpg", ".png"};
+    private   String [] files;
+    private   String path;
+    private   JSONObject jsonObject    = null;
+    private   String [] archive        = new String [] {".iso", ".gzip", ".zip", ".rar", ".tar.gz", ".7z", ".tgz", ".tar.xz", ".gz", ".jar", ".JAR"};
+    private   String [] music          = new String [] {".mp3", ".waw"};
+    private   String [] android        = new String [] {".apk"};
+    private   String [] deb            = new String [] {".deb"};
+    private   String [] exe            = new String [] {".exe", ".dll"};
+    private   String [] web            = new String [] {".php", ".js", ".html", ".css", ".phtml", ".htm", ".json"};
+    private   String [] desktop        = new String [] {".cpp", ".c", ".java", ".class", ".py"};
+    private   String [] image          = new String [] {".ico", ".jpg", ".png"};
+    private   String [] office         = new String [] {".txt", ".doc", ".docx", ".pdf", ".md", ".xls", ".djvu"};
+    private   String [] torrent        = new String [] {".torrent"};
 
-    CreateData (String pathDownload) {
-        this.downloads = pathDownload;
+    private String archives;
+    private String musics;
+    private String androids;
+    private String webs;
+    private String images;
+    private String offices;
+    private String debs;
+    private String exes;
+    private String desktops;
+    private String torrents;
+
+    CreateData () throws  IOException {
+        initJson();
     }
 
-    private void moveFile (final String[] nameType, String path) {
+    private void initJson () throws IOException {
+
+        try {
+            JSONParser jsonParser = new JSONParser();
+            File files = new File("/home/jashka/IdeaProjects/Catalog/src/settings.json");
+            Object object = jsonParser.parse(new FileReader(files));
+            jsonObject = (JSONObject) object;
+
+            JSONArray jsonArchives      = (JSONArray) jsonObject.get("archives");
+            JSONObject pathFolder       = (JSONObject) jsonObject.get("pathFolder");
+
+            downloads       = (String) jsonObject.get("pathDownloads");
+            archives        = (String) pathFolder.get("archive");
+            musics          = (String) pathFolder.get("music");
+            androids        = (String) pathFolder.get("android");
+            webs            = (String) pathFolder.get("web");
+            images          = (String) pathFolder.get("image");
+            offices         = (String) pathFolder.get("office");
+            debs            = (String) pathFolder.get("deb");
+            desktops        = (String) pathFolder.get("desktop");
+            exes            = (String) pathFolder.get("exe");
+            torrents        = (String) pathFolder.get("torrent");
+
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    private void moveFile (final String[] nameType, String path) throws IOException {
+
         File folder = new File(this.downloads);
         this.files = folder.list(new FilenameFilter() {
             @Override
@@ -30,7 +85,6 @@ public class CreateData {
             for ( String fileName : this.files) {
                 File oldFile    = new File(this.downloads + fileName);
                 File newFile    = new File(path + fileName);
-                if (!newFile.exists()) System.out.println("Путь " + newFile.getPath() + " не существует");
 
                 if (oldFile.renameTo(newFile)) {
                     System.out.print("Move file: " + fileName + "\n");
@@ -40,28 +94,48 @@ public class CreateData {
 
     }
 
-    protected void createData (int param) {
+    protected void createData (int param) throws IOException{
 
         switch (param) {
-            case 1:
-                path = "/home/jashka/Загрузки/Archive/";
-                this.moveFile(archives, path);
+            case 1 :
+                path = archives;
+                this.moveFile(archive, path);
                 break;
-            case 2:
-                path = "/home/jashka/Загрузки/Music/";
+            case 2 :
+                path = musics;
                 this.moveFile(music, path);
                 break;
-            case 3:
-                path = "/home/jashka/Загрузки/Android/";
+            case 3 :
+                path = androids;
                 this.moveFile(android, path);
                 break;
-            case 4:
-                path = "/home/jashka/Загрузки/Web/";
+            case 4 :
+                path = webs;
                 this.moveFile(web, path);
                 break;
-            case 5:
-                path = "/home/jashka/Загрузки/Images/";
-                this.moveFile(images, path);
+            case 5 :
+                path = images;
+                this.moveFile(image, path);
+                break;
+            case 6 :
+                path = offices;
+                this.moveFile(office, path);
+                break;
+            case 7 :
+                path = exes;
+                this.moveFile(exe, path);
+                break;
+            case 8 :
+                path = debs;
+                this.moveFile(deb, path);
+                break;
+            case 9 :
+                path = desktops;
+                this.moveFile(desktop, path);
+                break;
+            case 10 :
+                path = torrents;
+                this.moveFile(torrent, path);
                 break;
             default:
                 System.out.println("Неправильный ввод: " + param);
