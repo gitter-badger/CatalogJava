9f63cbd6a1efb06c92bc64fd9cdf083d2db831ba
Insidexa <nostresss77@gmail.com>
2014-08-03 20:30:49 +0300

    add new text

--- init.java
+++ init.java
@@ -10,15 +10,15 @@ public  class init  {
         CreateData data = new CreateData();
 
         System.out.println("1. Archive type \t(.iso, .gzip, .zip, .rar, .tar.gz, .7z, .tgz)");
-        System.out.println("2. Music type \t\t(.mp3, .waw)");
+        System.out.println("2. Music type \t\t(.mp3, .wav)");
         System.out.println("3. Android type \t(.apk)");
-        System.out.println("4. Web type \t\t(.php, .js, .html, .htm, .phtml, .json)");
+        System.out.println("4. Web type \t\t(.php, .js, .css .html, .htm, .phtml, .json)");
         System.out.println("5. Images type \t\t(.ico, .jpg, .png)");
         System.out.println("6. Office type\t\t(.doc, .docx, .txt, .djvu, .xls)");
         System.out.println("7. Exe type\t\t(.exe, .dll)");
         System.out.println("8. Deb type\t\t(.deb)");
         System.out.println("9. DevDesktop type\t(.cpp, .c, .java, .class, .py)");
-        System.out.println("10.Torrent type\t(.torrent)");
+        System.out.println("10.Torrent type\t\t(.torrent)");
         System.out.print("\nEnter the number: ");
 
         int s = scan.nextInt();
